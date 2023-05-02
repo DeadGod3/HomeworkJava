8fcd23a433328563ed695a0d59deb31ba86b0ea3
Ihor Pustovit <3flowerofsun3@gmail.com>
2023-05-02 18:46:27 +0200

    Practicing logging(JAVA LOGGER, log4j

---
+++ src/main/java/pustovit/homework/homework_29/LoggingPracticeIn26Homework.java
@@ -0,0 +1,16 @@
+package pustovit.homework.homework_29;
+
+
+public class LoggingPracticeIn26Homework {
+    public static void main(String[] args) {
+
+        System.err.println("Error"); // Example of creating an error (TEXT WITH RED COLOR)
+
+        one one = new one();
+        one.oneTwo(2);
+
+
+    }
+
+}
+
---
+++ src/main/java/pustovit/homework/homework_29/one.java
@@ -0,0 +1,14 @@
+package pustovit.homework.homework_29;
+
+import org.apache.log4j.Logger;
+
+public class one {
+    Logger logger = Logger.getLogger(one.class);
+
+    public void oneTwo(int oneNUMBER) {
+        if (oneNUMBER == 2) {
+            logger.debug("one.one . logger should be 1");
+        }
+    }
+
+}
