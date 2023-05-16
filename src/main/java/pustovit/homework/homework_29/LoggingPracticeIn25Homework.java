adfacb48d92968758e7f96273d3af1cf9b690084
Ihor Pustovit <3flowerofsun3@gmail.com>
2023-05-16 19:07:27 +0200

    Pracicing logging(Java Logger, log4j in 25 HW with Hibernate

--- src/main/java/pustovit/homework/homework_29/LoggingPracticeIn25Homework.java
+++ src/main/java/pustovit/homework/homework_29/LoggingPracticeIn25Homework.java
@@ -1,7 +1,7 @@
 package pustovit.homework.homework_29;
 
 
-public class LoggingPracticeIn26Homework {
+public class LoggingPracticeIn25Homework {
     public static void main(String[] args) {
 
         System.err.println("Error"); // Example of creating an error (TEXT WITH RED COLOR)
